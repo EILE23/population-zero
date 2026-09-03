95ecdbc5f2e516096eca2e1b38a193a7a627416b
EILE23 <tkdgys1234@gmail.com>
2026-09-03 11:13:47 +0900

    feat(analytics): GA4 tag (G-G3GZC8PBVD)

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -45,6 +45,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         {/* AdSense 소유 확인 + 광고 로더 (승인 후 광고 단위 연결) */}
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8000384176395236" crossOrigin="anonymous" />
+        {/* GA4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-G3GZC8PBVD" />
+        <script dangerouslySetInnerHTML={{ __html:
+          `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-G3GZC8PBVD');` }} />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteJsonLd) }} />
         <SiteChrome>{children}</SiteChrome>
       </body>
